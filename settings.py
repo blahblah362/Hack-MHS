e3ebc1ef071c2c7dc94ab88032d3dd02bb770d75
blahblah362 <134162398+blahblah362@users.noreply.github.com>
2023-05-20 13:44:44 -0400

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,24 @@
+import pygame.image
+from pygame.math import Vector2 as vec
+
+
+WIDTH, HEIGHT = 610, 670
+FPS = 60
+TOP_BOTTOM_BUFFER = 50
+MAZE_WIDTH, MAZE_HEIGHT = WIDTH-TOP_BOTTOM_BUFFER, HEIGHT-TOP_BOTTOM_BUFFER
+
+ROWS = 30
+COLS = 28
+
+
+BLACK = (0, 0, 0)
+RED = (208, 22, 22)
+GREY = (107, 107, 107)
+GREY_GREY = (30,30,30)
+WHITE = (255, 255, 255)
+PLAYER_COLOUR = (24,150,0)
+
+
+START_TEXT_SIZE = 30
+TEXT_SIZE = 15
+START_FONT = 'arima madurai'
